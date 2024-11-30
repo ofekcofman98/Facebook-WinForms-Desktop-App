c782ccf062c531e13f75c82efaa0d18171d4a2c7
ofek <ofekcofman98@gmail.com>
2024-11-30 19:57:41 +0200

    Reinitialize repository and add project files

---
+++ FacebookWinFormsApp/AppManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures
+{
+    internal class AppManager
+    {
+        private readonly string r_AppId = "272862089537667";
+
+
+
+    }
+}
