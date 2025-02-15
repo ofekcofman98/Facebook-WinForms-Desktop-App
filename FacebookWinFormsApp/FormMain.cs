fff43297fcdb6893d393d7911f6fedf2fadc6701
ofek <ofekcofman98@gmail.com>
2025-02-15 16:17:43 +0200

    errors

--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -716,7 +716,7 @@ namespace BasicFacebookFeatures
 
         private void buttonApplySearch_Click(object sender, EventArgs e)
         {
-            CompositeFilter filters = new CompositeFilter();
+            Filters filters = new Filters();
             if (comboBoxFriendList.SelectedItem == null)
             {
                 return;
@@ -732,8 +732,8 @@ namespace BasicFacebookFeatures
             }
             filters.AddFilter(new FilterRelationshipStatus(getUserSelectedRelationshipStatuses()));
             filters.AddFilter(new FilterLikedPages(getUserSelectedLikedPagesId()));
-            List<User> usersFriendList = selectedFriend.Friends.ToList();
-            List<User> filteredFriends = usersFriendList.FindAll(filters.Filter);
+
+            List<User> filteredFriends = filters.FilterUserFriends(selectedFriend);
             updateFilteredFriendsListBox(filteredFriends);
         }
 
